a9f4344716fc3672b6d784786d6e62562424b120
Jason Butwell <jasonbutwell@gmail.com>
2016-10-11 23:01:20 +0100

    Added code to start second activity from the first activity button click.

---
+++ app/src/main/java/com/jasonbutwell/multipleactivities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jasonbutwell.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void changeActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
